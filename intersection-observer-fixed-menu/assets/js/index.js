f86ef399368beeb803e6a8a15c48447d1f5207dc
KamilWojtalak <kamilwojtalak99@gmail.com>
2022-02-05 17:35:32 +0100

    [intersection-observer-fixed-menu] index.js add comments

--- intersection-observer-fixed-menu/assets/js/index.js
+++ intersection-observer-fixed-menu/assets/js/index.js
@@ -1,24 +1,32 @@
+// Turn on hot module replacement
 if (module.hot) {
   module.hot.accept();
 }
 
+// Function that is responsible for toggling fixed class for navigation
 (async function () {
+  // get our nav container and threshold
   const nav = document.querySelector(".header__nav");
   const sectionNavThreshold = document.querySelector("#section_1");
 
-  const NAV_TOGGLE_CLASS = 'header__nav_fixed';
+  // nav fixed class name
+  const NAV_FIXED_CLASS = 'header__nav_fixed';
 
+  // observer options objects
   const navObserverOptions = {
     root: null,
-    threshold: 0.9,
+    threshold: .9,
   };
 
-  const navObserver = new IntersectionObserver(function (entries, observer) {
-    // console.log(entries, observer);
+  // nav observer
+  const navObserver = new IntersectionObserver((entries, _) => {
+    // if is not intersecting return
     if(!entries?.[0]?.isIntersecting) return;
 
-    nav.classList.toggle(NAV_TOGGLE_CLASS)
+    // toggle nav class
+    nav.classList.toggle(NAV_FIXED_CLASS)
   }, navObserverOptions);
 
+  // observe first section
   navObserver.observe(sectionNavThreshold);
 })();
